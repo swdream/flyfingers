1d2422139d0cb2d64605e89646b693dc86cc4d96
swdream <ngtthanh1010@gmail.com>
2015-07-05 22:57:58 +0700

    refacetor

--- flyfingers/__init__.py
+++ flyfingers/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python2
 #!-*- coding: utf-8 -*-
-# Thurs Jun 2015
 
-import datetime
 import random
 import string
 import sys
@@ -10,6 +8,10 @@ import time
 
 __version__ = '0.1.1'
 __doc__ = '''Learn to type 10 fingers'''
+
+
+LIMITTIMES = 3
+STANDARDTIMES = 30
 
 
 def run(length):
@@ -20,25 +22,26 @@ def run(length):
         str_pattern = ''.join(random.choice(string.printable.split('\t')[0])
                               for i in range(int(length)))
         print str_pattern
-        starttime = datetime.datetime.now()
-        your_str = raw_input("Enter your string in 3 seconds (press Ctrl +" +
-                             "C to exit):\n")
-        endtime = datetime.datetime.now()
+        starttime = time.time()
+        your_str = raw_input("Enter your string in %d seconds" % LIMITTIMES +
+                             " (press Ctrl + C to exit):\n" )
+        endtime = time.time()
         yourtime = endtime - starttime
 
-        print "spent %s seconds" % yourtime.total_seconds()
+        print "Spent %s seconds" % yourtime
 
-        if yourtime.total_seconds() > 3:
-            print "so slow, need to learn more"
+        if yourtime > LIMITTIMES:
+            print "Too slow, need to learn more"
             sys.exit(1)
 
         if your_str == str_pattern:
             count = count + 1
             print "You are great."
-            print count
+            print "your passed times:", count
             print "-------------------------------------------------------"
-            if count == 30:
+            if count == STANDARDTIMES:
                 print "Congratulation, going to next level"
+                print "The number of characters will increase 1"
                 count = 0
                 length = length + 1
                 run(length)
